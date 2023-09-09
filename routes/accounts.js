afaa9d5b0a0405311f68e73ff9fd28ebb77c0cf3
peambeboy <peambeboy@gmail.com>
2023-09-09 19:40:35 +0700

    update put req

--- routes/accounts.js
+++ routes/accounts.js
@@ -30,28 +30,41 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
-router.put("/update/:id", (req, res, next) => {
-  const { id } = req.params;
-  const updatedData = {
-    ...req.body,
-    updated_at: DateTime.now()
-      .setZone("Asia/Bangkok")
-      .toFormat("yyyy-MM-dd HH:mm:ss"),
-  };
+router.put("/update/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const updatedData = {
+      ...req.body,
+      updated_at: DateTime.now()
+        .setZone("Asia/Bangkok")
+        .toFormat("yyyy-MM-dd HH:mm:ss"),
+    };
 
-  Users.findByIdAndUpdate(id, updatedData, { new: true })
-    .then((updatedUser) => {
-      if (!updatedUser) {
-        return res.status(404).json({ error: "User not found" });
-      }
-      console.log("User updated:", updatedUser);
-      res.json(updatedUser);
-    })
-    .catch((err) => {
-      console.error("Error while updating user:", err);
-      next(err);
+    // Check if the updated username or email already exists in the database
+    const existingUser = await Users.findOne({
+      $or: [{ username: updatedData.username }, { email: updatedData.email }],
+      _id: { $ne: id }, // Exclude the current user's ID from the search
     });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Username or email already exists" });
+    }
+
+    // Update the user
+    const updatedUser = await Users.findByIdAndUpdate(id, updatedData, { new: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    console.log("User updated:", updatedUser);
+    res.json(updatedUser);
+  } catch (err) {
+    console.error("Error while updating user:", err);
+    next(err);
+  }
 });
+
 
 router.delete("/delete/:id", (req, res, next) => {
   const { id } = req.params;
