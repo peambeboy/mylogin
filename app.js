f3c217ad7375a238afe7d8ce0ce875fe51cd336c
peambeboy <51479276+peambeboy@users.noreply.github.com>
2023-09-09 18:09:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000"
-}));
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
